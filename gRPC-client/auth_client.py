bbae1b2a2e5c1415e50ec2ae2a21c7241fc5f825
badrievad <zzdl4@bk.ru>
2024-11-11 13:26:00 +0300

    black formatted

--- gRPC-client/auth_client.py
+++ gRPC-client/auth_client.py
@@ -7,8 +7,8 @@ from protos import auth_pb2, auth_pb2_grpc
 def run():
     with grpc.insecure_channel("localhost:50051") as channel:
         stub = auth_pb2_grpc.AuthServiceStub(channel)
-        username = input('Введите логин: ')
-        password = input('Введите пароль: ')
+        username = input("Введите логин: ")
+        password = input("Введите пароль: ")
         response = stub.login(
             auth_pb2.LoginRequest(username=username, password=password)
         )
